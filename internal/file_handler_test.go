aa961ba15818e8daf241ce098984fe761bdc958c
Asher Duarte <dev81log@gmail.com>
2023-02-10 17:06:06 -0300

    create test UploadHandler and DownloadHandler

--- internal/file_handler_test.go
+++ internal/file_handler_test.go
@@ -25,3 +25,43 @@ func TestHtmlHandler(t *testing.T) {
 	}
 
 }
+
+func TestUploadHandler(t *testing.T) {
+	req, err := http.NewRequest("POST", "/upload", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(UploadHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusFound {
+		t.Errorf("handler retornou código de status errado: got %v want %v",
+			status, http.StatusFound)
+	}
+	if err != nil {
+		t.Errorf("erro retornado do ParseFiles: %v", err)
+	}
+
+}
+
+func TestDownloadHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/download", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(DownloadHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler retornou código de status errado: got %v want %v",
+			status, http.StatusOK)
+	}
+	if err != nil {
+		t.Errorf("erro retornado do ParseFiles: %v", err)
+	}
+
+}
