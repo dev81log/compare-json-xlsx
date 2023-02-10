da2f447e7e5715c7315723af3f42d5c6c992505a
Asher Duarte <dev81log@gmail.com>
2023-02-10 18:13:23 -0300

    adjust Redirect for final uploadHandler call

--- internal/file_handler.go
+++ internal/file_handler.go
@@ -9,54 +9,57 @@ import (
 )
 
 func HtmlHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("./templates/fileUpload.html")
+	tmp, err := template.ParseFiles("./templates/fileUpload.html")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	t.Execute(w, nil)
+	tmp.Execute(w, nil)
 }
+
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/download", http.StatusFound)
 	if r.Method != http.MethodPost {
 		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
 		return
 	}
 	r.ParseMultipartForm(32 << 20)
 
-	jsonFile, handler, err := r.FormFile("jsonFile")
+	jsonFile, jsonHandler, err := r.FormFile("jsonFile")
 	if err != nil {
 		http.Error(w, "Erro ao ler o arquivo json: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 	defer jsonFile.Close()
 
-	xlsxFile, handler2, err := r.FormFile("xlsxFile")
+	xlsxFile, xlsxHandler, err := r.FormFile("xlsxFile")
 	if err != nil {
 		http.Error(w, "Erro ao ler o arquivo xlsx: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 	defer xlsxFile.Close()
 
-	f, err := os.OpenFile("./upload/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	jsonOpen, err := os.OpenFile("./upload/"+jsonHandler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer f.Close()
+	defer jsonOpen.Close()
 
-	j, err := os.OpenFile("./upload/"+handler2.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	xlsxOpen, err := os.OpenFile("./upload/"+xlsxHandler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer j.Close()
+	defer xlsxOpen.Close()
 
-	io.Copy(f, jsonFile)
-	io.Copy(j, xlsxFile)
+	io.Copy(jsonOpen, jsonFile)
+	io.Copy(xlsxOpen, xlsxFile)
 
-	ConverterFiles()
+	defer ConverterFiles()                             // call converter function
+	http.Redirect(w, r, "/download", http.StatusFound) // redirect to download page
 }
+
 func DownloadHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Disposition", "attachment; filename=relatorio.xlsx")
 	http.ServeFile(w, r, "upload/relatorio.xlsx")
+
 	fmt.Println("Download concluído")
 }
